7c405a403f68d1763f135761fb37057eb28be61d
Ishant Mishra <dev.frontend.03@proton.me>
2025-06-14 17:41:31 +0545

    ProductController functionality added

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,6 +1,12 @@
 import express from 'express';
 const productRouter = express.Router();
-import { } from "../controllers/product.controller.js";
-import { protect } from "../middlewares/auth.middleware.js"
+import { addProduct, deleteProduct, getAllProducts, getLatestProducts, getProductById } from "../controllers/product.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
+
+productRouter.post('/', protect, addProduct);
+productRouter.delete('/:id', protect, deleteProduct);
+productRouter.get('/', getAllProducts);
+productRouter.get('/latest', getLatestProducts);
+productRouter.get('/by-id', getProductById);
 
 export default productRouter;
