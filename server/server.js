9be2e4bb85624450c1fd7fab903375e5ef63507b
Ishant Mishra <dev.frontend.03@proton.me>
2025-06-28 11:36:01 +0545

    Testing Payment with WebHook 3

--- server/server.js
+++ server/server.js
@@ -13,42 +13,28 @@ import orderRouter from './routes/order.routes.js';
 import paymentRouter from './routes/payment.routes.js';
 import adminRouter from './routes/admin.routes.js';
 
-//Configurations
 import connectDB from './config/db.config.js';
 await connectDB();
 
-//CORS
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5174',
-];
-
+// CORS
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    } else {
-      return callback(new Error('Not allowed by CORS'));
-    }
+    if (allowedOrigins.includes(origin)) return callback(null, true);
+    return callback(new Error('Not allowed by CORS'));
   },
-  credentials: true, // allows cookies to be sent
+  credentials: true,
 }));
 
-//Middlewares
+// ⚠️ Stripe Webhook BEFORE any body parser
+app.use("/api/webhook", express.raw({ type: "application/json" }), stripeWebhook);
+
+// Standard Middleware AFTER webhook
 app.use(express.json());
 app.use(cookieParser());
 
-//WebHook
-app.use(
-  "/api/webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhook
-);
-
-
-//Routes
+// Routes
 app.use('/api/auth', authRouter);
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
@@ -56,9 +42,8 @@ app.use('/api/order', orderRouter);
 app.use('/api/payment', paymentRouter);
 app.use('/api/admin', adminRouter);
 
-//CheckUP API
+// Root route
 app.get('/', (req, res) => { res.send("Klyora API Working...") });
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => { console.log(`Server is listening on http://localhost:${PORT}`) });
-
